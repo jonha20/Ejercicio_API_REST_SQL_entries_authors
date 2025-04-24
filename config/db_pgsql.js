79d8b73e36b73f4527bca06d4ed6a3da2c7ae613
jonha20 <64472507+jonha20@users.noreply.github.com>
2025-04-24 11:07:44 +0200

    fix/connectiondatabase

--- config/db_pgsql.js
+++ config/db_pgsql.js
@@ -1,15 +1,10 @@
 const { Pool } = require('pg');
 require('dotenv').config();
 
-const isRender = process.env.RENDER === 'true';
-
 const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_DATABASE,
-  password: process.env.DB_PASSWORD,
-  port: parseInt(process.env.DB_PORT),
-  ssl: isRender ? { rejectUnauthorized: false } : false,
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false }
 });
+
 
 module.exports = pool;
